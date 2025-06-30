741f5452212c7a9dc2c9cf588957abd2bbcf261d
Mrtn Anastasiya <martirosianam@mail.ru>
2025-06-30 16:48:45 +0300

    Внесены изменения в файлах с род. и доч. классами

--- src/js/bowman.js
+++ src/js/bowman.js
@@ -3,9 +3,10 @@ import Character  from './character.js';
 
 export default class Bowman extends Character {
     constructor(name, type) {
-        super(name, type, 100, 1, 25, 25);
-        if (this.type !== 'Bowman') {
-            throw new Error('Недопустимый тип персонажа. Выберите один из: Bowman, Swordsman, Magician, Daemon, Undead, Zombie');
-        }
+        super(name, type);
+
+        this.attack = 25;
+        this.defence = 25;
+
     }
 }
--- src/js/character.js
+++ src/js/character.js
@@ -1,18 +1,32 @@
 
 export default class Character {
-    constructor(name, type, health = 100, level = 1, attack, defence) {
-        if (new.target === Character) {
-            throw new Error('Нельзя создать экземпляр абстрактного класса');
+    constructor(name, type) {
+        const types = [
+            "Bowman",
+            "Swordsman",
+            "Magician",
+            "Daemon",
+            "Undead",
+            "Zombie",
+        ];
+
+        if (!types.includes(type)) {
+            throw new Error(
+                `Тип должен быть одним из перечисленных: ${types.join(", ")}`
+            );
         }
+
         if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
             throw new Error('Имя должно быть строкой длиной от 2 до 10 символов');
         }
 
         this.name = name;
         this.type = type;
-        this.health = health;
-        this.level = level;
-        this.attack = attack;
-        this.defence = defence;
+
+        this.health = 100;
+        this.level = 1;
+
+        this.attack = undefined;
+        this.defence = undefined;
     }
 }
